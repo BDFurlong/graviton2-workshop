4bface22ddfc0c64d222a9145a89f0e1ba18cdfe
Marcin Bednarz <mbz@amazon.com>
2023-11-20 22:58:53 -0700

    scripts updates

--- scripts/rds-peering-cleanup.sh
+++ scripts/rds-peering-cleanup.sh
@@ -16,21 +16,18 @@ export DefaultRouteCidr=$(aws ec2 describe-vpcs --filters "Name=vpc-id,Values=${
 echo $DefaultRouteCidr
 echo $GravitonRouteCidr
 
-### Create the VPC peering and accept the request
-export VpcPeeringId=$(aws ec2 create-vpc-peering-connection --vpc-id "$DefaultVpcId" --peer-vpc-id "$GravitonVpcId" --query VpcPeeringConnection.VpcPeeringConnectionId --output text)
-aws ec2 accept-vpc-peering-connection --vpc-peering-connection-id "$VpcPeeringId"
-aws ec2 create-tags --resources "$VpcPeeringId" --tags 'Key=Name,Value=GravitonID-VPCPeering'
-
-
 #### Graviton VPC CIDR / Source VPC route table config
-aws ec2 create-route --route-table-id "$DefaultRouteTableID" --destination-cidr-block "$GravitonRouteCidr" --vpc-peering-connection-id "$VpcPeeringId"
-aws ec2 create-route --route-table-id "$GravitonRouteTableID" --destination-cidr-block "$DefaultRouteCidr" --vpc-peering-connection-id "$VpcPeeringId"
+aws ec2 delete-route --route-table-id "$DefaultRouteTableID" --destination-cidr-block "$GravitonRouteCidr"
+aws ec2 delete-route --route-table-id "$GravitonRouteTableID" --destination-cidr-block "$DefaultRouteCidr"
 
 #### Add  routes to Graviton Private Subnets
 export GravitonPrivate1RouteTableID=$(aws ec2 describe-route-tables --filters 'Name=tag:Name,Values=GravitonID-base/BaseVPC/PrivateSubnet1' --query 'RouteTables[].Associations[].RouteTableId' --output json| jq -r '.[0]')
 export GravitonPrivate2RouteTableID=$(aws ec2 describe-route-tables --filters 'Name=tag:Name,Values=GravitonID-base/BaseVPC/PrivateSubnet2' --query 'RouteTables[].Associations[].RouteTableId' --output json| jq -r '.[0]')
 
-aws ec2 create-route --route-table-id "$GravitonPrivate1RouteTableID" --destination-cidr-block "$DefaultRouteCidr" --vpc-peering-connection-id "$VpcPeeringId"
-aws ec2 create-route --route-table-id "$GravitonPrivate2RouteTableID" --destination-cidr-block "$DefaultRouteCidr" --vpc-peering-connection-id "$VpcPeeringId"
+aws ec2 delete-route --route-table-id "$GravitonPrivate1RouteTableID" --destination-cidr-block "$DefaultRouteCidr" 
+aws ec2 delete-route --route-table-id "$GravitonPrivate2RouteTableID" --destination-cidr-block "$DefaultRouteCidr" 
 
+### Create the VPC peering and accept the request
+VpcPeeringId=$(aws ec2 describe-vpc-peering-connections --filters 'Name=tag:Name,Values=GravitonID-VPCPeering' --query 'VpcPeeringConnections[*].VpcPeeringConnectionId'  --output json | jq -r '.[0]')
 
+aws ec2 delete-vpc-peering-connection --vpc-peering-connection-id $VpcPeeringId
